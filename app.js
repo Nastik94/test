4bd7925c1532c4514fe57497051952447a675cda
Nastik94 <32068095+Nastik94@users.noreply.github.com>
2017-09-20 21:57:19 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ngRoute', 'ngCookies','pascalprecht.translate'])
+        .config(config)
+        .run(run);
+
+    config.$inject = ['$routeProvider', '$locationProvider','$translateProvider'];
+    function config($routeProvider, $locationProvider, $translateProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                templateUrl: 'home/view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'login/view.html',
+                controllerAs: 'vm'
+            })
+
+            .otherwise({ redirectTo: '/login' });
+
+        var en = {
+            "login" : "Login",
+            "password" : "Password",
+            "loginning" : "Log in",
+            "wellcome" : "Wellcome",
+            "logout" : "Log out"
+        };
+
+        var ru = {
+            "login" : "Логин",
+            "password" : "Пароль",
+            "loginning" : "Войти",
+            "wellcome" : "Добро пожаловать",
+            "logout" : "Выйти"
+        };
+
+        $translateProvider.translations('en',en);
+        $translateProvider.translations('ru',ru);
+        $translateProvider.preferredLanguage('ru');
+    }
+
+    run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
+    function run($rootScope, $location, $cookies, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookies.getObject('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/login');
+            }
+        });
+    }
+
+})();
---
+++ login/controller.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['UserService', '$location', 'AuthenticationService', '$rootScope', '$scope', '$translate'];
+    function LoginController(UserService, $location, AuthenticationService, $rootScope, $scope, $translate) {
+        var vm = this;
+        $scope.changeLanguage = function(lang){
+            $translate.use(lang);
+        }
+        vm.login = login;
+
+        function login() {
+            vm.dataLoading = true;
+            UserService.Create(vm.user)
+                .then(function (response) {
+                    if (!response.success) {
+                        vm.dataLoading = false;
+                    }
+                });
+            AuthenticationService.Login(vm.user.username, vm.user.password, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(vm.user.username, vm.user.password);
+                    $location.path('/');
+                } else {
+                    vm.dataLoading = false;
+                }
+            });
+        }
+    }
+
+})();
